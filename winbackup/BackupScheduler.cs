a5f38a9ac7f13a589bc88fc9131cc3164dedfc5e
Jesus Salas <jesusmamanisalas@gmail.com>
2026-05-31 22:41:32 -0500

    fix(RF4 D3): prevent concurrent timer ticks by using AutoReset=false + Monitor.TryEnter guard

--- winbackup/BackupScheduler.cs
+++ winbackup/BackupScheduler.cs
@@ -6,6 +6,8 @@ namespace winbackup
     public class BackupScheduler : IBackupScheduler
     {
         private System.Timers.Timer _timer;
+        private int _intervaloSegundos;
+        private readonly object _lockTick = new();
 
         public event Action OnTick;
         public bool EstaActivo => _timer?.Enabled ?? false;
@@ -13,10 +15,11 @@ namespace winbackup
         public void Iniciar(int intervaloSegundos)
         {
             Detener();
+            _intervaloSegundos = intervaloSegundos;
 
             _timer = new System.Timers.Timer(intervaloSegundos * 1000)
             {
-                AutoReset = true
+                AutoReset = false
             };
 
             _timer.Elapsed += Timer_Elapsed;
@@ -36,7 +39,23 @@ namespace winbackup
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            OnTick?.Invoke();
+            if (!Monitor.TryEnter(_lockTick))
+                return;
+
+            try
+            {
+                OnTick?.Invoke();
+            }
+            finally
+            {
+                Monitor.Exit(_lockTick);
+            }
+
+            if (_timer != null)
+            {
+                _timer.Interval = _intervaloSegundos * 1000;
+                _timer.Start();
+            }
         }
     }
 }
